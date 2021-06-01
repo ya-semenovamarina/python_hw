f305e2b5aba8086bbe06dee99a353635eb40a4c4
Ваше имя <semenovamarinai@icloud.com>
2021-06-01 22:09:12 +0300

    change letters, add finish string

--- task_2_5.py
+++ task_2_5.py
@@ -1,5 +1,6 @@
 price = [57.8, 46.51, 97, 11, 34.10, 34.78, 56, 9911, 10, 22]
 
 for prices in price:
-    r= int(prices)
-    kk = (prices - r) * 100
+    rub = int(prices)
+    kk = (prices - rub) * 100
+    print(f'{rub} руб {kk:02.0f} коп')
