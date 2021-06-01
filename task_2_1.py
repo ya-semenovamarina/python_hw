732d2d570914735080a348872a3f9a725a9d734f
Ваше имя <semenovamarinai@icloud.com>
2021-06-01 18:26:44 +0300

    add

---
+++ task_2_1.py
@@ -0,0 +1,8 @@
+a = 15 * 3
+b = 15 / 3
+c = 15 // 2
+d = 15 ** 2
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
---
+++ task_2_5.py
@@ -0,0 +1,5 @@
+price = [57.8, 46.51, 97, 11, 34.10, 34.78, 56, 9911, 10, 22]
+
+for prices in price:
+    r= int(prices)
+    kk = (prices - r) * 100
