4ccab155c7d083e819fd0509ada2fb92de866886
Ваше имя <semenovamarinai@icloud.com>
2021-06-10 18:16:24 +0300

    currency added

--- Semenova_Marina_dz_4/task_4_2.py
+++ Semenova_Marina_dz_4/task_4_2.py
@@ -1,14 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from xml.etree import ElementTree as etree
 
-page = 'http://www.cbr.ru/scripts/XML_daily.asp'
-source = requests.get(page).text
-print(dir(source))
-
-#def get_content(html):
-    #soup = BeautifulSoup(html, 'html.parser')
-    #items = soup.find_all('table', class_='data')
-
-
-
-
+with urlopen("http://www.cbr.ru/scripts/XML_daily.asp", timeout=10) as r:
+    USD_currency = print(etree.parse(r).findtext('.//Valute[@ID="R01235"]/Value'))
+with urlopen("http://www.cbr.ru/scripts/XML_daily.asp", timeout=10) as r:
+    EUR_currency = print(etree.parse(r).findtext('.//Valute[@ID="R01239"]/Value'))
