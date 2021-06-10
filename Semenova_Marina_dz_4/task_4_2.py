5bee527468e4bdb59cf5fe1534b231e76dba736b
Ваше имя <semenovamarinai@icloud.com>
2021-06-10 18:26:24 +0300

    currency added2

--- Semenova_Marina_dz_4/task_4_2.py
+++ Semenova_Marina_dz_4/task_4_2.py
@@ -1,6 +1,5 @@
 from urllib.request import urlopen
 from xml.etree import ElementTree as etree
-from datetime import datetime
 
 with urlopen("http://www.cbr.ru/scripts/XML_daily.asp") as r:
     USD_currency = print('USD', etree.parse(r).findtext('.//Valute[@ID="R01235"]/Value'))
